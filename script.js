7a73f833a5b8119a8f86c1cfdaab9841360f9229
henriqueoshikirigomes-blip <henrique.oshikiri.gomes@escola.pr.gov.br>
2026-03-12 11:55:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let button = document.getElementById("no");
-let height = window.innerHeight - 50;
-let width = window.innerWidth - 50;
 
-button.addEventListener("mouseover", function() {
-    button.style.position = "absolute";
-    button.style.top = Math.random() * height + "px";
-    button.style.left = Math.random() * width + "px";
-});
+function salvar() {
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    console.log("Nome:", nome);
+    console.log("Idade:", idade);
+}
